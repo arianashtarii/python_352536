0a6ea784f772b5afefaf944f7ae7741935d5ef01
arianashtarii <arian.ashtari11@gmail.com>
2025-10-20 22:08:48 +0330

    Add files via upload
    فایل های جلسه اول

---
+++ Session 1/assignment operator.py
@@ -0,0 +1,13 @@
+#assignment operator
+
+a=10
+
+print(a/2)
+
+a*3 #must be a=a*3......... a*3 not being calculated.....
+
+print(a)
+#-----------------------------------------------------------------------
+a=a*3 #written
+
+print(a)
