dab15e59b3b4dac004db8f6b8a944df158efafa9
arianashtarii <arian.ashtari11@gmail.com>
2025-10-29 19:10:25 +0330

    جلسه یازدهم کلاس پایتون

---
+++ Session11/Session11_01.py
@@ -0,0 +1,18 @@
+#ارور ها و خطا ها
+print("Start")
+
+# ValueError
+# age = int(input("Enter Age : "))
+# print(age)
+
+# FileNotFoundError
+# file = open("students.txt", "r")
+
+# IndexError
+# my_list= []
+# my_list[5] = "ahmad"
+
+# ZeroDivisionError
+# print(10/0)
+
+print("End")
