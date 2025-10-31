6a6ad0630924d1a075342718f2809edf272fe0a3
ARIAN-PC <arian.ashtari11@gmail.com>
2025-10-31 17:38:10 +0330

    آپدیت دوم

---
+++ Session11/Mini_Project_tkinter.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import messagebox
+#int   IntVar()
+#float   DoubleVar()
+#str StringVar()
+#bool BooleanVar()
+
+person_list=[]
+def save():
+    person={"id":id.get(),"name":name.get(),"family":family.get()}
+    person_list.append(person)
+    messagebox.showinfo("Saved","Saved Successfully")
+    id.set(0) #بعد از سیو کردن مقدار اتوماتیک صفر شود
+    name.set("") #بعد از سیو کردن مقدار خالی شود
+    family.set("") #بعد از سیو کردن مقدار خالی شود
+
+window = Tk() #پنجره خالی
+#window.config(background="black")
+
+window.title("Program Name") #اسم برنامه
+window.geometry("400x400") #اندازه برنامه
+
+#Id
+Label(window,text="Id:").place(x=10,y=10) #id default=0
+id=IntVar()
+Entry(window,textvariable=id,bg="lightblue").place(x=50,y=10)
+
+#Name
+Label(window,text="Name:").place(x=10,y=40)
+name=StringVar()
+Entry(window,textvariable=name,bg="lightblue").place(x=50,y=40)
+name.set("Arian")
+
+#Family
+Label(window,text="Family:").place(x=10,y=70)
+family=StringVar()
+Entry(window,textvariable=family,bg="lightblue").place(x=50,y=70)
+family.set("Ashtari") #یه مقدار ثابت پیش فرض میفرسته روی پنجره
+
+#Save Button
+Button(window,text="Save",command=save).place(x=10,y=100,width=100,height=100)
+
+window.mainloop() #برنامه تا زمانی که بسته نشده است ادامه دارد
---
+++ Session11/Session11_08.py
@@ -0,0 +1,10 @@
+#ایجاد پنجره خالی
+from tkinter import *
+
+window = Tk() #پنجره خالی
+
+window.title("Profile")
+window.geometry("300x300")
+
+
+window.mainloop() #تا موقعی که پنجره رو نبسته باشه ادامه داشته باشد
